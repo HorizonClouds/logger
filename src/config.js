a2f5c2bf97450ad04374ee827e78c9e8de1f2006
JaviFdez7 <javidelatorre400@gmail.com>
2025-01-03 11:13:19 +0100

    fix: backend port

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
-  backendPort: process.env.BACKEND_PORT || 6900,
+  backendPort: process.env.BACKEND_PORT || 6901,
   kafkaBroker: process.env.KAFKA_BROKER || 'localhost:9092',
   kafkaTopic: process.env.KAFKA_TOPIC || 'logs',
   logFilePath: process.env.LOG_FILE_PATH || 'src/logs/logfile.log'
